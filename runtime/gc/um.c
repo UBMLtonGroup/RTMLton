1946e6442b629e5bc1fed990613b1d14565cb1cd
Jeff Murphy <jcmurphy@jeffmurphy.org>
2015-04-13 11:40:17 +0000

    c stubs

---
+++ runtime/gc/um.c
@@ -0,0 +1,19 @@
+
+/*
+hello.1.c:(.text+0xb8fa): undefined reference to `UM_Header_alloc'
+hello.1.c:(.text+0xb912): undefined reference to `UM_Payload_alloc'
+hello.1.c:(.text+0xb92d): undefined reference to `UM_CPointer_offset'
+*/
+
+void
+UM_Header_alloc(Pointer umfrontier, Word64 w64)
+{
+
+}
+
+void
+UM_Payload_alloc(Pointer umfromtier, Word64 w64)
+{
+
+}
+
