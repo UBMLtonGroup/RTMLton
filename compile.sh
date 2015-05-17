ca38d75b275c7114666ae6f9af1c06f2abb8d401
Dan McArdle <demcardl@buffalo.edu>
2015-05-17 14:00:17 -0400

    added GC branch's compile script

---
+++ compile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+rm -rf build
+
+arch_flags=""
+make $arch_flags dirs     &&
+make $arch_flags runtime  &&
+make $arch_flags compiler &&
+make $arch_flags basis-no-check &&
+make $arch_flags script &&
+make $arch_flags mlbpathmap &&
+make $arch_flags constants 
+
+cat <<EOF
+Try:
+./build/bin/mlton -codegen c -keep g -gc-module none -verbose 3 hello.sml 
+./hello
+EOF
+
+
+exit 0
+
+TARGET=x86_64-pc-linux-gnu
+arch_flags="TARGET=$TARGET TARGET_OS=linux TARGET_ARCH=x86"
+make  dirs     &&
+cd runtime && make clean && cd .. &&
+make  runtime  &&
+make  compiler &&
+make  basis-no-check &&
+make  script &&
+make  mlbpathmap &&
+make  constants  &&
+cd runtime && make clean && cd .. &&
+mkdir build/lib/targets/$TARGET/ &&
+mkdir build/lib/targets/$TARGET/include &&
+mkdir build/lib/targets/$TARGET/sml &&
+make  $arch_flags runtime &&
+make constants &&
+make mlbpathmap
+
