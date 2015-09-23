3ba2a67dd1812427d70447d24c7b006dbf456761
bruce <leilmyxwz@gmail.com>
2015-09-23 01:00:29 -0400

    edits

--- x86-compile.sh
+++ x86-compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf *.c
-build/bin/mlton -codegen c -keep rssa -target x86_64-pc-linux-gnu -verbose 3 $1
-#gcc -pg -g3 -O2 -m32 *.c -o eq  -Ibuild/lib/targets/x86_64-pc-linux-gnu/include -Ibuild/lib/include -lgmp -Lbuild/lib/targets/x86_64-pc-linux-gnu/ -lmlton -lgdtoa-pic -l gdtoa -lgdtoa-gdb -lmlton-pic -lm
+build/bin/mlton -codegen c -keep g -target x86_64-pc-linux-gnu -verbose 3 $1
+gcc -O2 -m32 *.c -o eq  -Ibuild/lib/targets/x86_64-pc-linux-gnu/include -Ibuild/lib/include -lgmp -Lbuild/lib/targets/x86_64-pc-linux-gnu/ -lmlton -lgdtoa-pic -l gdtoa -lgdtoa-gdb -lmlton-pic -lm
