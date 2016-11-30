0897557390011daa2d4c4089f72ba5949a46d753
bhargavshivkumar <bhargavs@buffalo.edu>
2016-11-30 15:40:37 -0500

    gc bench tests

---
+++ ub/testgcb.sh
@@ -0,0 +1,3 @@
+../build/bin/mlton -const 'MLton.debug true' -verbose 2 -keep core-ml -keep xml -keep sxml -keep ssa -keep ssa2 -keep rssa -keep machine -keep g -codegen c -debug true testgcbench.sml
+#../build/bin/mlton -verbose 2 -keep core-ml -keep xml -keep sxml -keep ssa -keep ssa2 -keep rssa -keep machine -keep g -codegen c -debug true testgcbench.sml
+
